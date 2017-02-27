8418d24300621e0031c831b3293ef81c180b1436
vanderleiot <central@hostbdc.com.br>
2017-02-27 05:03:03 -0300

    pronto

---
+++ cliente/www/js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={	
+	'ApiUrl' : "http://eitafome.shopbdc.com.br/mobileapp/api",
+	'DialogDefaultTitle' : "Eita Fome",
+	'pushNotificationSenderid' : "76287014202",
+	'facebookAppId' : "661892507304842",
+	'APIHasKey' : "14700ffa73218ea25c45ff31d2faeca9"
+};
